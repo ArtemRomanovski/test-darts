851187d6462bb875db0f0a209ba7237c082f3847
Artem Romanovski <romanovski.art@gmail.com>
2021-03-15 17:16:00 +0300

    function upgarde and code reduction

--- src/app/components/game-page/game-page.component.ts
+++ src/app/components/game-page/game-page.component.ts
@@ -17,40 +17,54 @@ export interface pointArray {
 	styleUrls: ['./game-page.component.scss']
 })
 export class GamePageComponent implements OnInit {
+	public buttonActive_x1: boolean = false;
+	public buttonActive_x2: boolean = false;
+	public buttonActive_x3: boolean = false;
 	public countStep: number = 1;	// Count Step
-	public currantPointsArray = [	// Массив с попаданиями с трёх полей каждоко игрока
+	// Массив с попаданиями с трёх полей каждоко игрока
+	public currantPointsArray = [
 		[
-			{ dart: 0 },
-			{ dart: 0 },
-			{ dart: 0 },
-		],
+			{"dart": 0},
+			{"dart": 0},
+			{"dart": 0},
+		],	
 		[
-			{ dart: 0 },
-			{ dart: 0 },
-			{ dart: 0 },
-		],
+			{"dart": 0},
+			{"dart": 0},
+			{"dart": 0},
+		],	
 		[
-			{ dart: 0 },
-			{ dart: 0 },
-			{ dart: 0 },
-		]
+			{"dart": 0},
+			{"dart": 0},
+			{"dart": 0},
+		],	
 	];
-	public _User1_dart_1: number = 0;
-	public _User1_dart_2: number = 0;
-	public _User1_dart_3: number = 0;
-	public _User2_dart_1: number = 0;
-	public _User2_dart_2: number = 0;
-	public _User2_dart_3: number = 0;
-	public _User3_dart_1: number = 0;
-	public _User3_dart_2: number = 0;
-	public _User3_dart_3: number = 0;
+	// Временный массив для умножения
+	public userDartValue = [
+		[
+			{"dart": undefined},
+			{"dart": undefined},
+			{"dart": undefined},
+		],	
+		[
+			{"dart": undefined},
+			{"dart": undefined},
+			{"dart": undefined},
+		],	
+		[
+			{"dart": undefined},
+			{"dart": undefined},
+			{"dart": undefined},
+		],	
+	];
 	public titleStartInfo: string;	// starting info title
 	public titleStepInfo: string;	// info title Step
 	public usersRemaningPoints = [];
 	public gameOver = {
 		"victory_20_Steps": false,
 		"victory_30_Steps": false,
-		"draw": false,
+		"draw_20": false,
+		"draw_30": false,
 		"additionalRounds": false
 	};
 	public impossobility = {
@@ -79,10 +93,8 @@ export class GamePageComponent implements OnInit {
 		// reset `gameOver` value
 		this.gameOver.victory_20_Steps = false;
 		this.gameOver.victory_30_Steps = false;
-		this.gameOver.draw = false;
-		this.impossobility.status = false;
-		this.impossobility.user = "";
-		this.impossobility.point = 0;
+		this.gameOver.draw_20 = false;
+		this.gameOver.draw_30 = false;
 	};
 
 	public startGameUser() {
@@ -94,155 +106,47 @@ export class GamePageComponent implements OnInit {
 	};
 
 	public push() {
-		this.currantPointsArray[0][0].dart = this._User1_dart_1;
-		this.currantPointsArray[0][1].dart = this._User1_dart_2;
-		this.currantPointsArray[0][2].dart = this._User1_dart_3;
-		this.currantPointsArray[1][0].dart = this._User2_dart_1;
-		this.currantPointsArray[1][1].dart = this._User2_dart_2;
-		this.currantPointsArray[1][2].dart = this._User2_dart_3;
-		this.currantPointsArray[2][0].dart = this._User3_dart_1;
-		this.currantPointsArray[2][1].dart = this._User3_dart_2;
-		this.currantPointsArray[2][2].dart = this._User3_dart_3;
+		this.currantPointsArray.forEach((i, idx) => {
+			for(let t=0; t<this.currantPointsArray.length; t++) {
+				if(this.userDartValue[idx][t].dart == undefined) {
+					i[t].dart = 0;
+					console.log(i[t].dart);					
+				}
+				else i[t].dart = this.userDartValue[idx][t].dart;
+			};
+		});		
+		console.log(this.currantPointsArray);
 	};
 
 	public resetInput() {
-		this._User1_dart_1 = 0;
-		this._User1_dart_2 = 0;
-		this._User1_dart_3 = 0;
-		this._User2_dart_1 = 0;
-		this._User2_dart_2 = 0;
-		this._User2_dart_3 = 0;
-		this._User3_dart_1 = 0;
-		this._User3_dart_2 = 0;
-		this._User3_dart_3 = 0;
+		this.userDartValue.filter((i) => {
+			this.userDartValue.filter((j, jdx) => {
+				i[jdx].dart = undefined;
+			});
+		});
 	};
 
-	// 1 Gamer - 1 Dart (value multiplication)
-	public _User_1_addDart_1x1(value) {
-		this._User1_dart_1 = value;
-	};
-
-	public _User_1_addDart_1x2(value) {
-		this._User1_dart_1 = value * 2;
-	};
-
-	public _User_1_addDart_1x3(value) {
-		this._User1_dart_1 = value * 3;
-	};
-
-	// 1 Gamer - 2 Dart (value multiplication)
-	public _User_1_addDart_2x1(value) {
-		this._User1_dart_2 = value;
-	};
-
-	public _User_1_addDart_2x2(value) {
-		this._User1_dart_2 = value * 2;
-	};
-
-	public _User_1_addDart_2x3(value) {
-		this._User1_dart_2 = value * 3;
-	};
-
-	// 1 Gamer - 3 Dart (value multiplication)
-	public _User_1_addDart_3x1(value) {
-		this._User1_dart_3 = value;
-	};
-
-	public _User_1_addDart_3x2(value) {
-		this._User1_dart_3 = value * 2;
-	};
-
-	public _User_1_addDart_3x3(value) {
-		this._User1_dart_3 = value * 3;
-	};
-
-	// 2 Gamer - 1 Dart (value multiplication)
-	public _User_2_addDart_1x1(value) {
-		this._User2_dart_1 = value;
-	};
-
-	public _User_2_addDart_1x2(value) {
-		this._User2_dart_1 = value * 2;
-	};
-
-	public _User_2_addDart_1x3(value) {
-		this._User2_dart_1 = value * 3;
-	};
-
-	// 2 Gamer - 2 Dart (value multiplication)
-	public _User_2_addDart_2x1(value) {
-		this._User2_dart_2 = value;
-	};
-
-	public _User_2_addDart_2x2(value) {
-		this._User2_dart_2 = value * 2;
-	};
-
-	public _User_2_addDart_2x3(value) {
-		this._User2_dart_2 = value * 3;
-	};
-
-	// 2 Gamer - 3 Dart (value multiplication)
-	public _User_2_addDart_3x1(value) {
-		this._User2_dart_3 = value;
-	};
-
-	public _User_2_addDart_3x2(value) {
-		this._User2_dart_3 = value * 2;
-	};
-
-	public _User_2_addDart_3x3(value) {
-		this._User2_dart_3 = value * 3;
-	};
-
-	// 3 Gamer - 1 Dart (value multiplication)
-	public _User_3_addDart_1x1(value) {
-		this._User3_dart_1 = value;
-	};
-
-	public _User_3_addDart_1x2(value) {
-		this._User3_dart_1 = value * 2;
-	};
-
-	public _User_3_addDart_1x3(value) {
-		this._User3_dart_1 = value * 3;
-	};
-
-	// 3 Gamer - 2 Dart (value multiplication)
-	public _User_3_addDart_2x1(value) {
-		this._User3_dart_2 = value;
-	};
-
-	public _User_3_addDart_2x2(value) {
-		this._User3_dart_2 = value * 2;
-	};
-
-	public _User_3_addDart_2x3(value) {
-		this._User3_dart_2 = value * 3;
-	};
-
-	// 3 Gamer - 3 Dart (value multiplication)
-	public _User_3_addDart_3x1(value) {
-		this._User3_dart_3 = value;
-	};
-
-	public _User_3_addDart_3x2(value) {
-		this._User3_dart_3 = value * 2;
-	};
-
-	public _User_3_addDart_3x3(value) {
-		this._User3_dart_3 = value * 3;
+	public currantHitPoints(inputValue, idxUser, idxDart, factor) {
+		let result = inputValue*factor;
+		if(inputValue !== undefined) {
+			this.userDartValue[idxUser][idxDart].dart = result; 
+			console.log(this.userDartValue[idxUser][idxDart].dart);			 
+			console.log(`Очки - ${inputValue}`, `\nИгрок - ${idxUser}`,`\nИндекс(номер дротика) - ${idxDart}`, `\nМножитель - ${factor}`, `\nРезультат - ${result}`,`\n`,this.userDartValue[idxUser]);
+		};
 	};
 
 	// addition of points of three darts
 	public sumPoits() {
-		for (let i = 0; i < this.currantPointsArray.length; i++) {
-			let sum = 0;
-			for (let j = 0; j < this.currantPointsArray[i].length; j++) {
-				sum += +this.currantPointsArray[i][j].dart;
+		let sum;
+		this.currantPointsArray.forEach((i, idx) => {
+			sum = 0;
+			for(let j=0; j<3; j++) {
+				sum += i[j].dart;	
+				console.log(sum);							
 			};
-			this.userService.currantTotalPointsArray[i] = { totalPoint: sum };
-		};
+			this.userService.currantTotalPointsArray[idx] = { totalPoint: sum };
+			console.log(this.userService.currantTotalPointsArray[idx]);	
+		});
 	};
 
 	// Step
@@ -252,39 +156,41 @@ export class GamePageComponent implements OnInit {
 		this.addResultPointToPage();
 		// title info
 		this.titleStepInfo = `Текущие очки попаданий:\n${this.userService.usersGameArray[0].name}: ${(this.userService.currantTotalPointsArray[0].totalPoint)} (${this.currantPointsArray[0][0].dart}+${this.currantPointsArray[0][1].dart}+${this.currantPointsArray[0][2].dart})\n${this.userService.usersGameArray[1].name}: ${(this.userService.currantTotalPointsArray[1].totalPoint)} (${this.currantPointsArray[1][0].dart}+${this.currantPointsArray[1][1].dart}+${this.currantPointsArray[1][2].dart})\n${this.userService.usersGameArray[2].name}: ${(this.userService.currantTotalPointsArray[2].totalPoint)} (${this.currantPointsArray[2][0].dart}+${this.currantPointsArray[2][1].dart}+${this.currantPointsArray[2][2].dart})`;
+		console.log(this.titleStepInfo);
 	};
 
 	// add line to result table
 	public addResultPointToPage() {
 		this.usersRemaningPoints.push([]);
+		console.log(this.userService.usersGameArray);
+		console.log(this.userService.currantTotalPointsArray);
 		this.userService.usersGameArray.forEach((i, ind) => {
-			for (let j = 0; j < this.userService.usersGameArray.length; j++) {
-				if (ind == j) {
-					let x = i.points - (this.userService.currantTotalPointsArray[j].totalPoint);
-					if (x > 1) {
-						i.points = x;
-					}
-					else if (x == 0) {
-						i.points = x;
-						// alert(`${this.userService.usersGameArray[j].name} Winner!`);
-						this.gameOver.victory_20_Steps = true;
-						this.winnerUser = this.userService.usersGameArray[j].name;
-						break;
-					}
-					// else alert(`${this.userService.usersGameArray[j].name} набрал(а): ${this.userService.currantTotalPointsArray[j].totalPoint} очков - не сможет выйти по удвоению.`);
-					else {
-						this.impossobility.status = true;
-						this.impossobility.user = this.userService.usersGameArray[j].name;
-						this.impossobility.point = this.userService.currantTotalPointsArray[j].totalPoint;
-						setTimeout(() => {
-							this.impossobility.status = false;
-						}, 5000);
-					};
-					this.usersRemaningPoints[this.countStep - 1].push(
-						{ point: i.points, step: this.countStep }
-					);
-				};
+			let x = i.points-(this.userService.currantTotalPointsArray[ind].totalPoint);
+			console.log(x);
+			if (x > 1) {
+				i.points = x;
+			}
+			else if (x == 0) {
+				i.points = x;
+				// alert - Winner
+				this.gameOver.victory_20_Steps = true;
+				this.winnerUser = this.userService.usersGameArray[ind].name;
+			}
+			// alert - cannot win by doubling
+			else {
+				this.impossobility.status = true;
+				this.impossobility.user = this.userService.usersGameArray[ind].name;
+				this.impossobility.point = this.userService.currantTotalPointsArray[ind].totalPoint;
+				setTimeout(() => {
+					this.impossobility.status = false;
+					this.impossobility.user = "";
+					this.impossobility.point = 0;	
+				}, 5000);
 			};
+			// Table liner
+			this.usersRemaningPoints[this.countStep - 1].push(
+				{ point: i.points, step: this.countStep }
+			);	
 		});
 		// Step - 20
 		if (this.countStep === 20) {
@@ -296,12 +202,13 @@ export class GamePageComponent implements OnInit {
 					this.winnerUser = i.name;
 				};
 				if (i.points == min) {
-					// alert(`Ничья! Дополнительные 10 раундов!`);
+					// alert - Draw! Additional 10 Rounds!
 					this.gameOver.additionalRounds = true;
 					setTimeout(() => {
 						this.gameOver.additionalRounds = false;
 					}, 3000);
 				};
+				// showModal here
 			});
 		};
 		// Step 30
@@ -310,19 +217,27 @@ export class GamePageComponent implements OnInit {
 				var min = this.userService.usersGameArray[0].points;
 				if (i.points < min) {
 					min = i.points;
-					// alert(`Winner - ${i.name}!`);
+					// alert - Winner
+					this.gameOver.victory_30_Steps = true;
 					this.winnerUser = i.name;
-				};
-				if (i.points == min) {
-					// alert(`Игра окончена в ничью!`);
-					this.gameOver.draw === true;
+				}
+				else if (i.points == min) {
+					// alert - Draw
+					this.gameOver.draw_30 === true;
 				};
 			});
+			// showModal here
 		};
 		// Step
 		this.countStep++
 		this.resetInput();
 	};
+
+	// modal 
+	public showModal() {
+		
+	};
 };
+
 
 
